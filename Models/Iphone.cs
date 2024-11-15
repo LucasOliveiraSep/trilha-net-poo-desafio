bb59a52dcfc81a430cfcf7cb7f4da869d920230f
Lucas_Oliveira <150068160+LucasOliveiraSep@users.noreply.github.com>
2024-11-15 14:37:19 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+        public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
+        public Iphone(string numero, string modelo, int memoria) : base(numero, modelo, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Iphone.");
+        }
     }
 }
